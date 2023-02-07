555e6174669dbfd08d30536c71d9527c2978ede1
lhh <970519495@qq.com>
2023-02-07 10:24:11 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Button } from '@tarojs/components';
+// import { Button } from '@tarojs/components/dist-h5/react';
+// import { Loading } from 'retaroct-ui';
 import './App.css';
+console.log('Button: ', Button);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* <Button>Taro</Button> */}
+        {/* <Loading /> */}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
---
+++ src/config/index.ts
@@ -0,0 +1,4 @@
+export default () => {
+  const alias = {}
+  console.log('alias: ', alias);
+}
